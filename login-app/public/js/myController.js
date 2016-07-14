47b3222883057f3e82aaa436c4c7a39ecdfe43cb
Nguyen Nhat Thanh <harrynguyen131@gmail.com>
2016-07-14 10:03:17 +0700

    update on login and register forms. Display error message when fields are not filled in. And disable button when fields are not valid.

---
+++ login-app/public/js/myController.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp', []);
+// create angular controller
+myApp.controller('myController', function($scope) {
+  // function to submit the form after all validation has occurred
+  $scope.submitForm = function() {
+    // check to make sure the form is completely valid
+  };
+});
