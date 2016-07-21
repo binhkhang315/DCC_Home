f3bc3c0e473431526cba8ab596b84f77fea105e7
Vo Hong Lam <Vo Hong Lam>
2016-07-21 13:18:00 +0700

    fix code format in test/testcase.js

--- DCC/test/testcase.js
+++ DCC/test/testcase.js
@@ -12,143 +12,144 @@ function isEmpty(str) {
 }
 
 
-//Test for authentication
-describe("Test for Authentication #1", function ()
+describe("<Unit test for Login>", function ()
 {
-    return it("Login should fail if username and password are invalid ", function (done)
-    {
-        return server
+	describe("", function ()
+	{
+		return it("Test case 1 : Login should fail if username and password are invalid ", function (done)
+		{
+			return server
+				.post("/users/login")
+				.send(
+				{
+					username: "qwe",
+					password: "123"
+				})
+				.end(function (err, res)
+				{
+					assert.equal(res.headers.location, 'failure') // if login success, compare with "success" string, otherwise compare with "failure" string !!!!!
+					return done();
+				});
+				done();
+		});
+	});
+	
+	describe("", function ()
+	{
+		it("Test case 2 : Login should fail if username is valid but password is invalid ", function (done)
+		{
+			return server
+				.post("/users/login")
+				.send(
+				{
+					username: "anhnguyen",
+					password: "wrong_password"
+				})
+				.end(function (err, res)
+				{
+					assert.equal(res.headers.location, 'failure') 
+					return done();
+				});
+		});
+	});
+	
+	describe("", function ()
+	{
+		return it("Test case 3 : Login should fail if username is invalid but password is valid", function (done)
+		{
+			return server
+				.post("/users/login")
+				.send(
+				{
+					username: "wrong_username",
+					password: "dekvn123456"
+				})
+				.end(function (err, res)
+				{
+					assert.equal(res.headers.location, 'failure') 
+					return done();
+				});
+		});
+	});	
+	
+	describe("", function ()
+	{
+		return it("Test case 4 : Login should fail if username and pw are blank", function (done)
+		{
+			return server
+				.post("/users/login")
+				.send(
+				{
+					username: "",
+					password: ""
+				})
+				.end(function (err, res)
+				{
+					assert.equal(res.headers.location, 'failure') 
+					return done();
+				});
+		});
+	});
+	
+	describe("", function ()
+	{
+		return it("Test case 5 : Login should fail if username is blank and pw is valid", function (done)
+		{
+			return server
                 .post("/users/login")
                 .send(
-                        {
-                            username: "qwe",
-                            password: "123"
-                        })
-                .end(function (err, res)
-                {
-                    assert.equal(res.headers.location, 'failure') // if login success, compare with "success" string, otherwise compare with "failure" string !!!!!
-                    return done();
-                });
-                done();
-    });
-});
-
-
-describe("Test for Authentication #2", function ()
-{
-    it("Login should fail if username is valid but password is invalid ", function (done)
-    {
-        return server
-                .post("/users/login")
-                .send(
-                        {
-                            username: "anhnguyen",
-                            password: "wrong_password"
-                        })
-                .end(function (err, res)
-                {
-                    assert.equal(res.headers.location, 'failure') 
-                    return done();
-                });
-    });
-});
-
-describe("Test for Authentication #3", function ()
-{
-    return it("Login should fail if username is invalid but password is valid", function (done)
-    {
-        return server
-                .post("/users/login")
-                .send(
-                        {
-                            username: "wrong_username",
-                            password: "dekvn123456"
-                        })
-                .end(function (err, res)
-                {
-                    assert.equal(res.headers.location, 'failure') 
-                    return done();
-                });
-    });
-});
-
-describe("Test for Authentication #4", function ()
-{
-    return it("Login should fail if username and pw are blank", function (done)
-    {
-        return server
-                .post("/users/login")
-                .send(
-                        {
-                            username: "",
-                            password: ""
-                        })
-                .end(function (err, res)
-                {
-                    assert.equal(res.headers.location, 'failure') 
-                    return done();
-                });
-    });
-});
-
-describe("Test for Authentication #5", function ()
-{
-    return it("Login should fail if username is blank and pw is valid", function (done)
-    {
-        return server
-                .post("/users/login")
-                .send(
-                        {
-                            username: "",
-                            password: "dekvn123456"
-                        })
+                 {
+                    username: "",
+                    password: "dekvn123456"
+                })
                 .end(function (err, res)
                 {
                     assert.equal(res.headers.location, 'failure') 
                     done();
                 });
-    });
-});
-
-
-describe("Test for Authentication #6", function ()
-{
-    return it("Login should success if username and pw are valid", function (done)
-    {
-        return server
+		});
+	});
+	
+	describe("", function ()
+	{
+		return it("Test case 6 : Login should success if username and pw are valid", function (done)
+		{
+			return server
                 .post("/users/login")
                 .send(
-                        {
-                            username: "anhnguyen",
-                            password: "dekvn123456"
-                        })
+                    {
+                    username: "anhnguyen",
+                    password: "dekvn123456"
+                    })
                 .end(function (err, res)
                 {
                     assert.equal(res.headers.location, 'success') 
                     return done();
                 });
-    });
-});
-
-
-describe("Test for Authentication #7", function ()
-{
-    return it("Login should fail if username is valid, password is blank", function (done)
-    {
+		});
+	});	
+	
+	describe("", function ()
+	{
+		return it("Test case 7 : Login should fail if username is valid, password is blank", function (done)
+		{
         return server
                 .post("/users/login")
                 .send(
-                        {
-                            username: "anhnguyen",
-                            password: ""
-                        })
+                {
+                    username: "anhnguyen",
+                    password: ""
+                 })
                 .end(function (err, res)
                 {
                     assert.equal(res.headers.location, 'failure') 
                     return done();
                 });
-    });
+		});
+	});	
 });
+
+//-----------------------------------------------------------------------
 
 describe("<Unit Test Register >", function ()
 {
