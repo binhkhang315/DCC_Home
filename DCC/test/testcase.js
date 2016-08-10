5e39afd862a307636ff7ea87dc6d6afe861772ce
Tung <votantung@gmail.com>
2016-08-10 14:53:54 +0700

    add basic testcase for /models/course.js|usersdata.js

--- DCC/test/testcase.js
+++ DCC/test/testcase.js
@@ -177,16 +177,23 @@ describe('<Unit test for Login>', function() {
 });
 describe('<Unit test for Course model', function() {
     describe('Method Course', function() {
-        return it('Test case 1: getCourseByID', function(done) {
+        return it('Test case 1: getCourseByID with id existed in database - return course object', function(done) {
             models.course.getCourseByID('1', function(course) {
                 assert.equal(course.id, 1);
                 return done();
             })
         });
     });
-
     describe('', function() {
-        return it('Test case 2: getCourseByName', function(done) {
+        return it('Test case 2: getCourseByID with id not found in database - return null' , function(done) {
+            models.course.getCourseByID('not found', function(course) {
+                assert.equal(course, null);
+                return done();
+            })
+        });
+    });
+    describe('', function() {
+        return it('Test case 3: getCourseByName with name existed in database - return course course object', function(done) {
             models.course.getCourseByName('CBA Overview', function(course) {
                 assert.equal(course.name, 'CBA Overview');
                 return done();
@@ -194,7 +201,15 @@ describe('<Unit test for Course model', function() {
         });
     });
     describe('', function() {
-        return it('Test case 3: getCourseByCategory', function(done) {
+        return it('Test case 4: getCourseByName with name not found in database - return course = null', function(done) {
+            models.course.getCourseByName('noy found', function(course) {
+                assert.equal(course, null);
+                return done();
+            })
+        });
+    });
+    describe('', function() {
+        return it('Test case 5: getCourseByCategory with category existed in database - return course[] list object ', function(done) {
             models.course.getCourseByCategory('CBA Overview', function(course) {
                 assert.equal(course[0].category, 'CBA Overview');
                 assert.equal(course[2].category, 'CBA Overview');
@@ -202,36 +217,59 @@ describe('<Unit test for Course model', function() {
             })
         });
     });
-
     describe('', function() {
-        return it('Test case 4: getCourseByTrainerID', function(done) {
+        return it('Test case 6: getCourseByCategory with category not found in database - return course[] null ', function(done) {
+            models.course.getCourseByCategory('not found', function(course) {
+                assert.equal(course[0], null);
+                return done();
+            })
+        });
+    });
+    describe('', function() {
+        return it('Test case 7: getCourseByTrainerID with trainerID existed in database - return course[] list object', function(done) {
             models.course.getCourseByTrainerID('King Nguyen', function(course) {
                 assert.equal(course[0].trainerID, 'King Nguyen');
                 return done();
             })
         });
     });
+    describe('', function() {
+        return it('Test case 8: getCourseByTrainerID with TrainerID not found in database - return course[] null' , function(done) {
+            models.course.getCourseByTrainerID('not found', function(course) {
+                assert.equal(course[0], null);
+                return done();
+            })
+        });
+    });
+    describe('', function() {
+        return it('Test case 9: getCourseList in database - return course[] list object' , function(done) {
+            models.course.getCourseList( function(course) {
+                assert.equal(course[0].id, '1');
+                return done();
+            })
+        });
+    });
 });
 
-// describe('<Unit test for User model', function() {
-//     describe('Method User', function() {
-//         return it('Test case 1: getUserByID', function(done) {
-//             models.User.getUserByID('1', function(user) {
-//                 assert.equal(user.username, 'admin');
-//                 return done();
-//             })
-//         });
-//     });
+describe('<Unit test for User model', function() {
+    describe('Method User', function() {
+        return it('Test case 1: getUserByID', function(done) {
+            models.User.getUserByID('1', function(user) {
+                assert.equal(user.username, 'admin');
+                return done();
+            })
+        });
+    });
 
-//     describe('', function() {
-//         return it('Test case 2: getUserByName', function(done) {
-//             models.User.getUserByName('admin', function(user) {
-//                 assert.equal(user.id, '1');
-//                 return done();
-//             })
-//         });
-//     });
-// });
+    describe('', function() {
+        return it('Test case 2: getUserByName', function(done) {
+            models.User.getUserByName('admin', function(user) {
+                assert.equal(user.id, '1');
+                return done();
+            })
+        });
+    });
+});
 //-----------------------------------------------------------------------
 
 
