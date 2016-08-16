619919264023462c13815e99823e2c260398e655
Tran Cong Truc <congtruc94@gmail.com>
2016-08-16 10:49:08 +0700

    fix edit modal not show

--- DCC/public/js/dist/textAngularSetup.js
+++ DCC/public/js/dist/textAngularSetup.js
@@ -100,10 +100,10 @@ angular.module('textAngularSetup', [])
 	//
 	keyMappings : [],
 	toolbar: [
-		['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
-		['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
-		['justifyLeft','justifyCenter','justifyRight','justifyFull','indent','outdent'],
-		['html', 'insertImage', 'insertLink', 'insertVideo', 'wordcount', 'charcount']
+		['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'quote'],
+		['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol'],
+		['justifyLeft','justifyCenter','justifyRight','justifyFull'],
+		['html', 'insertImage', 'insertLink', 'insertVideo']
 	],
 	classes: {
 		focussed: "focussed",
