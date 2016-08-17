353a4ea9f8214d967046672c0426a47f47c443eb
Tung <votantung@gmail.com>
2016-08-17 14:49:36 +0700

    add calendar UI to views, start using google calendar API method

--- DCC/angular/controller.js
+++ DCC/angular/controller.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngCookies', 'ngTagsInput', 'textAngular','course']);
+var myApp = angular.module('myApp', ['ngCookies', 'ngTagsInput', 'textAngular', 'ngMaterial', 'materialCalendar','course']);
 // creat angular controller
 myApp.controller('LoginCtrl', function($scope, $http, $cookies, $rootScope) {
     // function to submit the form after all validation has occurred
@@ -218,4 +218,53 @@ myApp.controller('FeedbackCtrl', function($scope, $http, $window) {
       }).then(function(result){
         $scope.average = result.data.result;
       });
+});
+
+myApp.controller("calendarCtrl", function($scope, $filter, $http, $q) {
+
+    $scope.dayFormat = "d";
+
+    // To select a single date, make sure the ngModel is not an array.
+    $scope.selectedDate = null;
+
+    // If you want multi-date select, initialize it as an array.
+    $scope.selectedDate = [];
+
+    $scope.firstDayOfWeek = 0; // First day of the week, 0 for Sunday, 1 for Monday, etc.
+    $scope.setDirection = function(direction) {
+      $scope.direction = direction;
+      $scope.dayFormat = direction === "vertical" ? "EEEE, MMMM d" : "d";
+    };
+
+    $scope.dayClick = function(date) {
+      $scope.msg = "You clicked " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
+    };
+
+    $scope.prevMonth = function(data) {
+      $scope.msg = "You clicked (prev) month " + data.month + ", " + data.year;
+    };
+
+    $scope.nextMonth = function(data) {
+      $scope.msg = "You clicked (next) month " + data.month + ", " + data.year;
+    };
+
+    $scope.tooltips = true;
+    $scope.setDayContent = function(date) {
+
+        // You would inject any HTML you wanted for
+        // that particular date here.
+        return "<p></p>";
+
+        // You could also use an $http function directly.
+        return $http.get("/some/external/api");
+
+        // You could also use a promise.
+        var deferred = $q.defer();
+        $timeout(function() {
+            deferred.resolve("<p></p>");
+        }, 1000);
+        return deferred.promise;
+
+    };
+
 });
