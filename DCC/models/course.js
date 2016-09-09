a28fa135e88238f1143126b7a529d31734f98192
Dinh Quang Trung <dqtrung118@gmail.com>
2016-09-09 09:35:20 +0700

    Edited variable name and syntax in Model.Course

--- DCC/models/course.js
+++ DCC/models/course.js
@@ -6,52 +6,66 @@ var opts = {
     dateFormat: 'YYYY.MM.DD'
 };
 var log = require('simple-node-logger').createLogManager(opts).createLogger();
-var rq = require('./DataObjects');
-var course = new rq.Course.Course();
+var DataObjects = require('./DataObjects');
+var _course = new DataObjects.Course.Course();
 
 module.exports = function(sequelize, DataTypes)
 {
-    var Course = sequelize.define('course', course, {
-        classMethods: {
-            getCourses: function(cb) {
-                log.info('/models/course: getCourses() : ');
-                var query = {
-                    where: {
-                        isDeleted: false
-                    }
-                };
-                Course.findAll(query).then(cb);
-            },
-            getByID: function(id, cb) {
-                log.info('/models/course: getByID() : ' + id);
-                var query = {
-                    where: {
-                        id: id,
-                        isDeleted: false
-                    }
-                };
-                Course.findOne(query).then(cb);
-            },
-            getByName: function(name, cb) {
-                log.info('/models/course: getByName() : ' + name);
-                var query = {
-                    where: {
-                        name: name,
-                        isDeleted: false
-                    }
-                };
-                Course.findOne(query).then(cb);
-            },
-            getByCategory: function(category, cb) {
-                log.info('/models/course: getByCategory() : ' + category);
-                var query = {
-                    where: {
-                        category: category,
-                        isDeleted: false
-                    }
-                };
-                Course.findAll(query).then(cb);
-            },
+  var Course = sequelize.define('course', _course,
+  {
+    classMethods:
+    {
+      getCourses: function(cb)
+      {
+        log.info('/models/course: getCourses() : ');
+        var query =
+        {
+          where:
+          {
+            isDeleted: false
+          }
+        };
+        Course.findAll(query).then(cb);
+      },
+      getByID: function(id, cb)
+      {
+        log.info('/models/course: getByID() : ' + id);
+        var query =
+        {
+          where:
+          {
+            id: id,
+            isDeleted: false
+          }
+        };
+        Course.findOne(query).then(cb);
+      },
+      getByName: function(name, cb)
+      {
+        log.info('/models/course: getByName() : ' + name);
+        var query =
+        {
+          where:
+          {
+            name: name,
+            isDeleted: false
+          }
+        };
+        Course.findOne(query).then(cb);
+      },
+      getByCategory: function(category, cb)
+      {
+        log.info('/models/course: getByCategory() : ' + category);
+        var query =
+        {
+          where:
+          {
+            category: category,
+            isDeleted: false
+          }
+        };
+        Course.findAll(query).then(cb);
+      },
             // getByTrainer: function(trainer, cb) {
             //     log.info('/models/course: getByTrainer() : ' + trainer);
             //     var query = {
@@ -62,8 +76,8 @@ module.exports = function(sequelize, DataTypes)
             //     };
             //     Course.findAll(query).then(cb);
             // },
-        },
-        tableName: 'Courses'
-    });
-    return Course;
+    },
+    tableName: 'Courses'
+  });
+  return Course;
 };
