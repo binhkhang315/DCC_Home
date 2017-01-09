45f86b7a9232892d46f0b609622dda8c9ccddbe1
ericviston <ericviston@gmail.com>
2017-01-09 16:52:49 +0700

    finish trainee doashboard -show all course of a trainee

--- DCC/client/partials/traineeModule/dashboard/dashboard.js
+++ DCC/client/partials/traineeModule/dashboard/dashboard.js
@@ -18,15 +18,9 @@ myApp.config(function($stateProvider) {
 myApp.factory('dashboardServices', ['$http', function($http) {
 
     var factoryDefinitions = {
-      getTodaysStats: function() {
-        return $http.get('partials/traineeModule/dashboard/mock/todays_stats.json').success(function(data) { return data; });
+      getMyTraingPrograms: function() {
+        return $http.get('partials/traineeModule/dashboard/mock/myTrainingprogram.json').success(function(data) { return data; });
       },
-	  getRecentNews: function() {
-        return $http.get('partials/traineeModule/dashboard/mock/recent_news.json').success(function(data, status, headers, config) { return data; });
-      },
-	  getLastFiveCustomers: function() {
-        return $http.get('partials/traineeModule/dashboard/mock/customers_last_five.json').success(function(data) { return data; });
-      }
 	}
 
     return factoryDefinitions;
@@ -34,20 +28,17 @@ myApp.factory('dashboardServices', ['$http', function($http) {
 ]);
 
 //Controllers
-myApp.controller('todaysStatsController', ['$scope', 'dashboardServices', function($scope, dashboardServices) {
-	dashboardServices.getTodaysStats().then(function(result){
-		$scope.data = result.data;
-	});
-}]);
-
-myApp.controller('recentNewsController', ['$scope', 'dashboardServices', function($scope, dashboardServices) {
-	dashboardServices.getRecentNews().then(function(result){
-		$scope.data = result.data;
-	});
-}]);
-
-myApp.controller('getLastFiveCustomersController', ['$scope', 'dashboardServices', function($scope, dashboardServices) {
-	dashboardServices.getLastFiveCustomers().then(function(result){
-		$scope.data = result.data;
+myApp.controller('MyCoursesCtrl', ['$scope', 'dashboardServices', function($scope, dashboardServices) {
+	dashboardServices.getMyTraingPrograms().then(function(result){
+        result.data.forEach(traningProgram => {
+            traningProgram.myCourseList.forEach(course => {
+                if (course.status == 'Enrolled') {course.backgroundColor = '#4FC3F7'}
+                else if (course.status == 'Learning') {course.backgroundColor = '#F4FF81'}
+                else if (course.status == 'Not test') {course.backgroundColor = '#F44336'}
+                else if (course.status == 'Learned') {course.backgroundColor = '#8BC34A'}
+                else {course.backgroundColor = '#4FC3F7'}
+            });
+        });
+		$scope.myTrainingProgramList = result.data;
 	});
 }]);
